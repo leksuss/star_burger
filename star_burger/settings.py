169a46513510ab100a54fc962e4f672f17ceeab0
Andrey Alekseev <leksus@leksus.info>
2023-07-14 23:49:50 +0300

    add settings by default

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,7 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -64,7 +64,7 @@ DEBUG_TOOLBAR_PANELS = [
 
 ROLLBAR = {
     'access_token': env('ROLLBAR_ACCESS_TOKEN'),
-    'environment': env('ENVIRONMENT'),
+    'environment': env('ENVIRONMENT', 'development'),
     'code_version': '1.0',
     'root': BASE_DIR,
 }
@@ -94,7 +94,7 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env('DB_URL')
+        default=env('DB_URL', 'sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
     )
 }
 
