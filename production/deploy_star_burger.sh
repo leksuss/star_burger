c9f170c9a804b53cf427e6a0e20e8b7c773b8fe9
Andrey Alekseev <leksus@leksus.info>
2023-11-05 19:40:49 +0300

    fix newline in finished message

--- production/deploy_star_burger.sh
+++ production/deploy_star_burger.sh
@@ -19,4 +19,4 @@ systemctl reload nginx.service
 last_commit=$(git rev-parse HEAD)
 curl -H "X-Rollbar-Access-Token: ${ROLLBAR_ACCESS_TOKEN}" -H "Content-Type: application/json" -X POST "https://api.rollbar.com/api/1/deploy" -d '{"environment": "production", "revision": "'${last_commit}'", "rollbar_name": "leksus", "local_username": "root", "comment": "Yet Another Deployment", "status": "succeeded"}'
 
-echo "\nstar-burger was successfully delployed"
+echo -e "\nstar-burger was successfully delployed"
